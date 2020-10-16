0367c007244f114be64658e1eb6863ab3d8fe4db
nanato12 <admin@nanato12.info>
2020-10-16 17:45:37 +0900

    Fix init.sh ・bashに変更 ・sdksをマウントする場合、rm -rfできないので、gitを使用しリポジトリからファイルを取ってくる

--- tweaks/init.sh
+++ tweaks/init.sh
@@ -1,14 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
+cd
 # Get the toolchain
 curl https://kabiroberai.com/toolchain/download.php?toolchain=ios-linux -Lo toolchain.tar.gz && \
     tar xzf toolchain.tar.gz -C ${THEOS}/toolchain && \
     rm toolchain.tar.gz
 
 # Get an iOS SDK
-rm -r ${THEOS}/sdks && \
-    git clone --recursive https://github.com/theos/sdks.git ${THEOS}/sdks
+cd ${THEOS}/sdks/ && \
+    git init && \
+    git remote add origin https://github.com/theos/sdks.git && \
+    git pull origin master
 
+cd
 # Install the Swift toolchain
 curl https://kabiroberai.com/toolchain/download.php?toolchain=swift-ubuntu-latest -Lo swift-toolchain.tar.gz && \
     tar xzf swift-toolchain.tar.gz -C ${THEOS}/toolchain && \
